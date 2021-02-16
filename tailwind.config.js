13895dc0a12b83c9a0d4a6929ac4cce72bdd50a2
Tony Ketcham <ketcham@pnw.edu>
2021-02-15 22:56:56 -0600

    Add credit + documentation for defaultExtractor

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,8 @@ module.exports = {
     content: ['./src/**/*.svelte', './src/**/*.html'],
     options: {
       keyframes: true,
+      // considers dynamic class bindings when purging unused classes
+      // credit: https://github.com/matebek https://dev.to/matebek
       defaultExtractor: (content) => [
         ...(content.match(/[^<>"'`\s]*[^<>"'`\s:]/g) || []),
         ...(content.match(/(?<=class:)[^=>\/\s]*/g) || []),
